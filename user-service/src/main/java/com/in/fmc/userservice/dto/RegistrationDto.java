ad73aa204ad152954a677e018efc78c6447a9ab6
Prarabdh Chauhan <chauhan.prarabdh@gmail.com>
2021-07-08 16:07:18 +0530

    Added models for user-service

---
+++ user-service/src/main/java/com/in/fmc/userservice/dto/RegistrationDto.java
@@ -0,0 +1,54 @@
+package com.in.fmc.userservice.dto;
+
+public class RegistrationDto {
+
+	private String name;
+
+	private String emailId;
+
+	private String mobileNo;
+
+	private String username;
+
+	private String password;
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
