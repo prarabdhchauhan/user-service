3e7378e113ecbd1d328aaa2abecee55e39b97b2a
Prarabdh Chauhan <chauhan.prarabdh@gmail.com>
2021-07-08 17:36:11 +0530

    Models updated in user-service

--- user-service/src/main/java/com/in/fmc/userservice/dto/RegistrationDto.java
+++ user-service/src/main/java/com/in/fmc/userservice/dto/RegistrationDto.java
@@ -1,5 +1,9 @@
 package com.in.fmc.userservice.dto;
 
+import lombok.Data;
+
+@Data
+
 public class RegistrationDto {
 
 	private String name;
@@ -11,44 +15,9 @@ public class RegistrationDto {
 	private String username;
 
 	private String password;
+	
+	
 
-	public String getEmailId() {
-		return emailId;
-	}
-
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-
-	public String getMobileNo() {
-		return mobileNo;
-	}
-
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 
 }
--- user-service/src/main/java/com/in/fmc/userservice/entities/Register.java
+++ user-service/src/main/java/com/in/fmc/userservice/entities/Register.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "REGISTER")
 public class Register {
 
@@ -20,38 +23,6 @@ public class Register {
 	private String emailId;
 
 	@Column(name = "mobile_no")
-	private String mobileNo;
-
-	public String getEmailId() {
-		return emailId;
-	}
-
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-
-	public String getMobileNo() {
-		return mobileNo;
-	}
-
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+	private Long mobileNo;
 
 }
